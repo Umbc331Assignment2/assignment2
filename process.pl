65a76317f291155651a27655b3a83e13b8e610bf
ayebear <ayebear@gmail.com>
2014-09-22 00:29:06 -0400

    Fixed indentation, now ONLY SPACES. Note: Please don't add tabs anymore, unless we ONLY use tabs

--- process.pl
+++ process.pl
@@ -24,8 +24,8 @@ my @patterns = (
     # Match any datum that contains at least two double letters in a row:
     qr/\S*(\p{L})\1(\p{L})\2\S*/,
 
-	# Matches Digits
-	qr/^\-?\d*\d$/,
+    # Matches Digits
+    qr/^\-?\d*\d$/,
 
     # match any datum that is a same character repeated a (NOT) prime number times
     # Derived from http://montreal.pm.org/tech/neil_kandalgaonkar.shtml
@@ -38,22 +38,22 @@ my @rulefreq = (0,0,0,0,0);
 #arrays to store values
 my @rule1 = (); my @rule2 = (); my @rule3 = (); my @rule4 = (); my @rule5 = (); 
 # Hashmap for storing the matched datums with their respective rule
-my %matches = (	
-				1 => \@rule1, 			#Backslashs are C equivalent of &
-				2 => \@rule2,
-				3 => \@rule3,
-				4 => \@rule4,
-				5 => \@rule5,
-			  						); #key then value respectively
+my %matches = ( 
+                1 => \@rule1,           #Backslashs are C equivalent of &
+                2 => \@rule2,
+                3 => \@rule3,
+                4 => \@rule4,
+                5 => \@rule5,
+                                    ); #key then value respectively
 
 # TODO: Make sure these are named correctly
 my @ruleNames = (
-	"Greek and Cyrilli letter",
-	"Balanced square brackets",
-	"Double letters",
-	"Digits",
-	"Prime length"
-	);
+    "Greek and Cyrilli letter",
+    "Balanced square brackets",
+    "Double letters",
+    "Digits",
+    "Prime length"
+    );
 
 # Scalar for storing most recent integer seen
 my $integer = "empty";
@@ -64,33 +64,33 @@ while ($input = <STDIN>) {
     # Breaks it up into data chunks delimited by whitespace:
     @datums = split('\s+', $input);
     foreach my $datum (@datums) {
-		#print $datum; print "\n";
+        #print $datum; print "\n";
         # Capture basic patterns
         # TODO: Clean this up some with a loop
         # TODO: Don't store matches?
         if ($datum =~ $patterns[0]) {
-			push(@{$matches{1}},$datum);
+            push(@{$matches{1}},$datum);
             $rulefreq[0]++;
         }
-		if ($datum =~ $patterns[1]) {
-			push(@{$matches{2}},$datum);
+        if ($datum =~ $patterns[1]) {
+            push(@{$matches{2}},$datum);
             $rulefreq[1]++;
         }
-		if ($datum =~ $patterns[2]) {
-			push(@{$matches{3}},$datum);
+        if ($datum =~ $patterns[2]) {
+            push(@{$matches{3}},$datum);
             $rulefreq[2]++;
         }
-		if ($datum =~ $patterns[3]) {	#TODO make sure doesnt remember first integer (if first happens to be biggest)
-			
-			if (($integer ne "empty") && ($datum > $integer)) {
-				push(@{$matches{4}},$datum);
-				$rulefreq[3]++;
+        if ($datum =~ $patterns[3]) {   #TODO make sure doesnt remember first integer (if first happens to be biggest)
+            
+            if (($integer ne "empty") && ($datum > $integer)) {
+                push(@{$matches{4}},$datum);
+                $rulefreq[3]++;
             }
             $integer = $datum;
         }
         #first regex makes sure its just letters second check is if its NOT prime third is if its a single char
-		if ($datum =~ qr/^([a-zA-Z])\1+$/ and $datum !~ $patterns[4] and $datum !~ qr/^[a-zA-Z]$/) { 
-			push(@{$matches{5}},$datum);
+        if ($datum =~ qr/^([a-zA-Z])\1+$/ and $datum !~ $patterns[4] and $datum !~ qr/^[a-zA-Z]$/) { 
+            push(@{$matches{5}},$datum);
             $rulefreq[4]++;
         }
 
@@ -103,7 +103,7 @@ while ($input = <STDIN>) {
 my %rulefreqHash;
 my $counter = 0;
 foreach (@rulefreq) {
-	$rulefreqHash{$counter++} = $_;
+    $rulefreqHash{$counter++} = $_;
 }
 
 # Sort the rule frequencies by value
@@ -112,41 +112,37 @@ foreach (@rulefreq) {
 
 # Print the results!
 foreach (@sortedRulefreq) {
-	print $ruleNames[$_], ": ", $rulefreq[$_], "\n";
+    print $ruleNames[$_], ": ", $rulefreq[$_], "\n";
 }
 
 #foreach my $item (@rule4) {print $item;print "\n";}
 #foreach my $key (keys %matches) {
-#	foreach($matches{$key}) {
-#	print @_;
-#	}
+#   foreach($matches{$key}) {
+#   print @_;
+#   }
 #}
 
 
 sub printsorted {
-	my (%matches, @rulefreq) = @_;
-	$biggestval = 0;
-	$biggestindex = 0;
-	print "KLJSDG";
-	for($i=0; $i < scalar @rulefreq ; $i++) {
-		if($rulefreq[$i] >= $biggestval) {
-			$biggestindex = $i;
-		}
-	}#end for
+    my (%matches, @rulefreq) = @_;
+    $biggestval = 0;
+    $biggestindex = 0;
+    print "KLJSDG";
+    for($i=0; $i < scalar @rulefreq ; $i++) {
+        if($rulefreq[$i] >= $biggestval) {
+            $biggestindex = $i;
+        }
+    }#end for
 
-	foreach my $item (@{$matches{$biggestindex}}) {	#prints rule's data collected
-		print $item; print "\n";
-	}
-											
-	delete $matches{$biggestindex};				#removes rule we just printed
-	delete $rulefreq[$biggestindex];				#keeps both structures same size
+    foreach my $item (@{$matches{$biggestindex}}) { #prints rule's data collected
+        print $item; print "\n";
+    }
+                                            
+    delete $matches{$biggestindex};             #removes rule we just printed
+    delete $rulefreq[$biggestindex];                #keeps both structures same size
 }#end printsorted
 
 # Print each match:
 #while (!keys %matches) {
-#	printsorted(\%matches, @rulefreq);	#idea is each time its called with one less key
-#}										#always prints the biggest value
-
-
-
-
+#   printsorted(\%matches, @rulefreq);  #idea is each time its called with one less key
+#}                                      #always prints the biggest value
