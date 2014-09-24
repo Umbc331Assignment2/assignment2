16c00e40f3c88e834334733d598c4d127ab87b90
hudak1 <hudak1@umbc.edu>
2014-09-23 22:24:07 -0400

    formatted POD description better

--- process.pl
+++ process.pl
@@ -31,18 +31,18 @@ B<perl> B<process.pl> B<<> I<input.txt>
 
 =head1 DESCRIPTION
 
-Given a valid UTF-8 encoded text file comprised of whitespace-delimited
-data, this program find the following things:
-  1. Any datum starting with a Greek character and ending with a
-     Cyrillic one.
-  2. Any datum that contains a balanced set of square brackets.
-  3. Any datum that contains at least two distinct double letters in a
-     row.
-  4. Any integer that is larger than the most recent integer seen.
-  5. Any datum that is a same character repeated a prime number of times.
-Once all these data are extracted from the corpus, the program prints a
-list of the frequencies of these categories of datums, from most to
-least frequent.
+ Given a valid UTF-8 encoded text file comprised of whitespace-delimited
+ data, this program find the following things:
+   1. Any datum starting with a Greek character and ending with a
+      Cyrillic one.
+   2. Any datum that contains a balanced set of square brackets.
+   3. Any datum that contains at least two distinct double letters in a
+      row.
+   4. Any integer that is larger than the most recent integer seen.
+   5. Any datum that is a same character repeated a prime number of times.
+ Once all these data are extracted from the corpus, the program prints a
+ list of the frequencies of these categories of datums, from most to
+ least frequent.
 
 =head1 REQUIRES
 
